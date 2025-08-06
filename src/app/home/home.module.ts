59d1b3f28c2207adb8e2dcc3d22fd815ac448620
Cadu <dudueduardosantos2018@gmail.com>
2025-08-06 15:21:48 -0300

    Adiciona import de css para o funcionamento do leaflete e refatora código

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { LeafletMapComponent } from "../component/leaflet-map/leaflet-map.component";
 
 
 @NgModule({
@@ -12,8 +13,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
-  ],
+    HomePageRoutingModule,
+    LeafletMapComponent
+],
   declarations: [HomePage]
 })
 export class HomePageModule {}
